ed706983096001654f228ed6b1572c537ba65ba6
Sanh <44762454+NghiSanh@users.noreply.github.com>
2019-09-13 14:49:38 +0900

    Upload7

---
+++ Ex1.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Ex2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ex1
+{
+    public static void main(){
+
+        int n = 0;
+        String str = Integer.toString(n);
+        
+        int n1 = 98;
+        String str1 = Integer.toString(n1);
+            
+        System.out.println(str + str1);
+    }
+}
